0fccff5059b60adf000627a3f1e98b313d7c20a6
lhseunge <lhseunge@gmail.com>
2024-12-09 19:55:14 +0900

    git init

---
+++ src/main/java/com/fun/grpc_practice/GrpcPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.fun.grpc_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fun/grpc_practice/GrpcPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fun.grpc_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
